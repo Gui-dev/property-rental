9e839ef3cbfa5d44af51774d64381a738a914be7
Gui-dev <adrianon10@gmail.com>
2020-12-24 02:57:42 -0300

    feat: add StatusBar

--- App.tsx
+++ App.tsx
@@ -1,30 +1,28 @@
+import 'react-native-gesture-handler'
 import React from 'react'
 import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
 import AppLoading from 'expo-app-loading'
-import { useFonts, Montserrat_400Regular, Montserrat_700Bold } from '@expo-google-fonts/montserrat'
+import { useFonts, Montserrat_400Regular, Montserrat_500Medium, Montserrat_700Bold } from '@expo-google-fonts/montserrat'
+
+import { Routes } from './src/routes'
 
 export default function App() {
 
-  const [fontsLoaded] = useFonts([Montserrat_400Regular, Montserrat_700Bold])
+  const [fontsLoaded] = useFonts({
+    Montserrat_400Regular,
+    Montserrat_500Medium,
+    Montserrat_700Bold
+  })
 
   if (!fontsLoaded) {
     return <AppLoading />
   }
 
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar style="dark" backgroundColor="#222" translucent={ false } />
+      <Routes />
+    </>
   )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-})
