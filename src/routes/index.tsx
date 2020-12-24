8ac9ac8f952f428be5340520cd69e85bacbdbda5
Gui-dev <adrianon10@gmail.com>
2020-12-24 03:13:05 -0300

    feat: add fonts and icons

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
+import { Feather } from '@expo/vector-icons'
 
 import { Home } from '../screens/Home'
 import { Details } from '../screens/Details'
+import { TouchableOpacity } from 'react-native-gesture-handler'
 
 export const Routes: React.FC = () => {
 
@@ -11,9 +13,52 @@ export const Routes: React.FC = () => {
 
   return (
     <NavigationContainer>
-      <Navigator>
-        <Screen name="Home" component={ Home }/>
-        <Screen name="Details" component={ Details }/>
+      <Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          headerTitleStyle: {
+            fontFamily: 'Montserrat_700Bold',
+            color: '#FFF'
+          },
+          headerStyle: {
+            backgroundColor: '#222'
+          }
+        }}
+      >
+        <Screen
+          name="Home"
+          component={ Home }
+          options={{
+            headerTitle: "Alugue",
+            headerRight: () => {
+              return (
+                <TouchableOpacity
+                  style={{ marginRight: 15 }}
+                  activeOpacity={ .6 }
+                >
+                  <Feather name="shopping-bag" size={ 24 } color="#FFF"/>
+                </TouchableOpacity>
+              )
+            }
+          }}
+        />
+        <Screen
+          name="Details"
+          component={ Details }
+          options={{
+            headerTitle: "Detalhe",
+            headerRight: () => {
+              return (
+                <TouchableOpacity
+                  style={{ marginRight: 15 }}
+                  activeOpacity={ .6 }
+                >
+                  <Feather name="shopping-bag" size={ 24 } color="#FFF"/>
+                </TouchableOpacity>
+              )
+            }
+          }}
+        />
       </Navigator>
     </NavigationContainer>
   )
